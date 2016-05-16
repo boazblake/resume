8701aa9e3775abb6ec79fc5a58e811c1e2ab02d6
boazblake <boazblake@gmail.com>
2016-05-16 11:24:29 -0500

    cleaning up any staged and uncomitted changes

--- js/views.js
+++ js/views.js
@@ -7,78 +7,13 @@ import Backbone from 'backbone'
 //MODULES
 
 var Header = React.createClass({
-<<<<<<< HEAD
-
-	 getInitialState: function () {
-        return {hover: false};
-=======
 	 
 	 getInitialState: function() {
         return {hover: false}
->>>>>>> refrac
     },
 
     mouseOver: function (evt) {
     	evt.preventDefault()
-<<<<<<< HEAD
-    	var classNameofIcon = evt.currentTarget.className
-    	console.log('evt', evt.currentTarget.className)
-
-        this.setState({hover: true});
-    },
-
-    mouseOut: function () {
-        this.setState({hover: false});
-    },
-
-	render:function(){	
-		if (this.state.hover){
-			var divstyle = {
-					backgroundColor:'rgba(41, 128, 185,1.0)',
-					color:'white',
-					transition:'0.7s all ease'
-			}	
-		} else {
-			var divstyle = {
-					backgroundColor:'white',
-					color:'rgba(41, 128, 185,1.0)',
-					transition:'0.7s all ease'
-		}
-	}
-		
-
-		return (
-			<div className='hed'>
-					<h1>Boaz Blake</h1>
-					<table>
-						<tr className='faRow'>
-							<th>
-								<i style={divstyle} onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}  className='icons envelope fa fa-envelope-o'></i>
-							</th>
-							<th>
-								<i style={divstyle} onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} className="icons mobile fa fa-mobile"></i>
-							</th>
-							<th>
-								<i style={divstyle} onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} className="icons linkedin fa fa-linkedin-square"></i>
-							</th>
-							<th>
-								<i style={divstyle} onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} className="icons github fa fa-github-square"></i>
-							</th>
-						</tr>
-						<tr className='faRow'>
-								<th style={divstyle} onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} className='titles envelope' >email</th>
-								<th style={divstyle} onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} className='titles mobile' >mobile</th>
-								<th style={divstyle} onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} className='titles linkedin' >linkedin</th>
-								<th style={divstyle} onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} className='titles github' >portfolio</th>
-						</tr>
-						<tr>
-								<td style={divstyle} onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} className='deets envelope' >boazblake@gmail.com</td>
-								<td style={divstyle} onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} className='deets mobile' >347.420.3251</td>
-								<td style={divstyle} onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} className='deets linkedin' >www.linkedin.com/in/boazblake</td>
-								<td style={divstyle} onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} className='deets github' >boazblake.github.io/portfolio</td>
-						</tr>
-					</table>
-=======
     	var fullName = evt.currentTarget.className.split(' ')
     	var target = fullName.slice(5)
     	console.log('target', target)
@@ -239,14 +174,11 @@ var Header = React.createClass({
 							</td>
 					</tr>
 				</table>
->>>>>>> refrac
 			</div>
 		)
 	}
 })
 
-<<<<<<< HEAD
-=======
 var SubHeader = React.createClass({
 
 
@@ -260,30 +192,11 @@ var SubHeader = React.createClass({
 			)
 	}
 })
->>>>>>> refrac
 
 var Footer = React.createClass({
 	render: function(){
 		return (
 			<div className='row footer'>
-				SUBHEADER
-			</div>
-		)
-	}
-})
-
-var SubHeader = React.createClass({
-
-
-	render: function(){
-
-		function _setDimensions(){
-			var subHeader = document.querySelector('.subHeader')
-			console.log('subHeader', subHeader)
-		}
-
-		return (
-			<div className='row subHeader'>
 
 			</div>
 		)
@@ -299,31 +212,6 @@ var Education = React.createClass({
 					<th>{edu.title}</th>
 					<tbody>
 						<tr>
-<<<<<<< HEAD
-						<td className='desc'><span>Front End Engineering </span>
-	Intensive JavaScript programming course with a focus on SOLID principles and MVC frameworks ReactJS, BackboneJs
-	The Iron Yard - Houston</td>
-							<td className='date'>February 2016 - April 2016</td>
-						</tr>
-						<tr>
-						<td className='desc'><span>PhD Human Space Exploration  </span> environemntal stress on epigenetic inheritance and health
-	University of Houston
-	NASA HRP Grant #NNX12AF04G</td>
-							<td className='date'>September 2011-May 2014 (on Haitus)</td>
-						</tr>
-						<tr>
-						<td className='desc'><span>Bachelor of Science in Sports Medicine </span>
-	Intensive program focused on Athletic injuries to both the  amateur and professional athlete
-	James Madison University, Harrisonburg, VA
-	cum laude</td>
-							<td className='date'>August 2007 - May 2010</td>
-						</tr>
-						<tr>
-						<td className='desc'><span>International Criminal Justice </span>
-	John Jay College of Criminal Justice, New York, NY
-	GPA: 4.0.</td>
-							<td className='date'>January 2006 - August 2007</td>
-=======
 							<td className='eight columns desc'>{edu.body.tr1.td1.desc}</td>
 							<td className='four columns date'>{edu.body.tr1.td1.date}</td>
 						</tr>
@@ -338,7 +226,6 @@ var Education = React.createClass({
 						<tr>
 							<td className='eight columns desc'>{edu.body.tr4.td1.desc}</td>
 							<td className='four columns date'>{edu.body.tr4.td1.date}</td>
->>>>>>> refrac
 						</tr>
 					</tbody>
 				</table>
@@ -359,17 +246,9 @@ var Skills = React.createClass({
 							<th>{skills.title.t2}</th>
 						</tr>
 						<tr>
-<<<<<<< HEAD
-						<td><span>Coding </span> nodeJs, backboneJS, reactJs, jQuery, HTML, SCSS, Github,
-						<span>Adobe  </span> Photoshop, illustrator, InDesign After Effects, DreamWeaver.
-						<span>Open Source </span> Sketch, iDraw, inkScape, Sketch, Gimp.
-						<span>3D </span> 123D Design, 123D Make, Live Interior 3D, Sketchup.</td>
-							<td>I have started to research PHP postgreSQL, as well as Redux Flux and nodeJS</td>
-=======
 							<td>{skills.body.td1}</td>
 							<td>{skills.body.td2}</td>
 							<td>{skills.body.td3}</td>
->>>>>>> refrac
 						</tr>
 					</tbody>
 				</table>
@@ -387,41 +266,6 @@ var Work = React.createClass({
 					<th>{work.title}</th>
 					<tbody>
 						<tr>
-<<<<<<< HEAD
-							<td  className='desc'><span>Freelance Graphic Designer </span>
-	Houston, Texas
-	Designed and implemented marketing strategies and campaigns for local businesses. Designed logos  and branding.</td>
-							<td
-	className='date'>April 2015 - Present</td>
-						</tr>
-						<tr>
-						<td className='desc'><span>Range Safety Officer </span> 
-	Top Gun Range, Houston, Texas
-	Educate public and private sector market on all aspects of  gun ownership and usage. Focus on tactical application with integration of armed and unarmed situations.</td>
-							<td className='date'>April 2015 - Present</td>
-						</tr>
-						<tr>
-						<td className='desc'><span>PhD Research Assistant </span> 
-	Taught Undergraduate science classNamees on Biomechanics, Statistics and health and fitness while studying the effects of spaceflight on the nervous system and the genetic effects of environmental stressors on immune function.</td>
-							<td className='date'>September 2011 - May 2014</td>
-						</tr>
-						<tr>
-						<td className='desc'><span>Graduate Athletic Trainer - Syracuse Football Team </span>
-	Syracuse University, Syracuse, NY
-	Evaluated, wrote and implemented football injury rehabilitation protocols.</td>
-							<td className='date'>June 2010 - August 2011</td>
-						</tr>
-						<tr>
-						<td className='desc'><span>Athletic Training Student  - Athletic Training Education Program </span>
-	James Madison University, Harrisonburg, VA
-	Rotations with Men's Soccer, Men's Basketball, Football, Women's Track and Field and Cross Country, Women's Swimming and Diving, Men's and Women's Tennis.</td>
-							<td className='date'>September 2008 - May 2010</td>
-						</tr>
-						<tr>
-						<td className='desc'><span>Anti-Terrorist Officer/Bodyguard </span>
-	Provided close protection services for individuals and teams traveling throughout Israel.</td>
-							<td className='date'>October 2004 - December 2005</td>
-=======
 							<td className='eight columns desc'>{work.body.tr1.td1.desc}</td>
 							<td	className='four columns date'>{work.body.tr1.td1.date}</td>
 						</tr>
@@ -448,7 +292,6 @@ var Work = React.createClass({
 						<tr>
 							<td className='eight columns desc'>{work.body.tr7.td1.desc}</td>
 							<td className='four columns date'>{work.body.tr7.td1.date}</td>
->>>>>>> refrac
 						</tr>
 					</tbody>
 				</table>
@@ -576,11 +419,7 @@ var Resume = React.createClass({
 			<div id='render'>
 				<Header/>
 				<SubHeader/>
-<<<<<<< HEAD
-				<MainBody/>
-=======
 				<MainBody mainBody={this.props.ResumeData.mainBody} />
->>>>>>> refrac
 				<Footer/>
 			</div>
 		)
